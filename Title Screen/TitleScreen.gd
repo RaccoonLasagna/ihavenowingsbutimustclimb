b8d8365648e2e7f8e4bca96b9528a4ffeeea29fb
RaccoonLasagna <102906286+RaccoonLasagna@users.noreply.github.com>
2026-02-21 22:17:03 +0000

    changed starting screen to title screen
    and scale to 1

--- Title Screen/TitleScreen.gd
+++ Title Screen/TitleScreen.gd
@@ -2,7 +2,7 @@ extends Control
 
 
 func _on_start_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Tall paralax level/TALL_Map.tscn")
+	get_tree().change_scene_to_file("res://map.tscn")
 	pass # Replace with function body.
 
 
