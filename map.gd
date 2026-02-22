db5d545fc667461e9abdf32c97e6ea10996dc7bb
S_tikit <3135308T@student.gla.ac.uk>
2026-02-22 15:12:33 +0000

    final

--- map.gd
+++ map.gd
@@ -6,5 +6,5 @@ extends Node2D
 const CAM_SPEED =  4.0
 func _process(delta: float) -> void:
 	camera.position = camera.position.lerp(player.position, delta * CAM_SPEED)
-	camera.position.x = clamp(camera.position.x, 600, 8510)
+	camera.position.x = clamp(camera.position.x, 600, 9000)
 	camera.position.y = clamp(camera.position.y, -9999, 0)
